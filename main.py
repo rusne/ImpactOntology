1d36d73ebdfb52435efb958439f6b67ed0e965bc
Rusne Sileryte <rusne.sileryte@gmail.com>
2021-01-26 10:07:50 +0100

    ontologies for the local impact assessment

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 from owlready2 import *
 from functions import *
 
+
 onto_path.append('ontologies/')
-# onto_asmfa = get_ontology('http://www.semanticweb.org/AS-MFA').load()
-# onto = get_ontology('http://www.semanticweb.org/spatial').load()
-ontoImp = get_ontology('http://www.semanticweb.org/impact').load()
-ontoSpa = get_ontology('http://www.semanticweb.org/spatial').load()
+ontoImp = get_ontology('http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact').load()
+ontoSpa = get_ontology('http://www.semanticweb.org/rusnesileryte/ontologies/2018/3/spatial').load()
 
-
-# onto = get_ontology("http://test.org/dynamic_onto.owl")
-# onto.imported_ontologies.append(onto_asmfa)
-# onto.imported_ontologies.append(onto_spatial)
-# onto.imported_ontologies.append(onto_impact)
-# print(onto.imported_ontologies)
 
 def where_is(what, onto):
 
     locs = []
-    for item in onto.search(is_a=what):
+    for item in ontontoSpa.search(is_a=what):
         for loc in item.hasLocation.indirect():
             locs.append(loc.draw())
     write_shp('output/{0}'.format(what.name),
@@ -48,29 +41,35 @@ with ontoSpa:
     import ontology_spatial
     ontology_spatial.load(ontoSpa)
 
-    class Entity(Thing):
-        equivalent_to = [ontoImp.Actor |
-                         ontoImp.Emission |
-                         ontoImp.Population
-                         ]
 
-    # class hasAnchor(ObjectProperty):
-    #     equivalent_to = [ontoImp.hasSource]
-    #
-    # class hasRelation(ObjectProperty):
-    #     equivalent_to = [ontoImp.hasFootprint]
+    class Entity(Thing):
+        equivalent_to = [ontoSpa.ContextElement |
+                         ontoSpa.Effect]
+
+    class hasAnchor(ObjectProperty):
+        equivalent_to = [ontoSpa.hasRoot]
+
+    class hasRelation(ObjectProperty):
+        equivalent_to = [ontoSpa.hasFootprint]
+
+    class hasLocation(ObjectProperty):
+        equivalent_to = [ontoSpa.isAt,
+                         ontoSpa.occursAt]
+
+    class SpatialRelation(ontoSpa.Relation):
+        equivalent_to = [ontoSpa.Footprint]
 
 
 
 # ####### Accessibility ####### #
 
-# access_effect = ContainerAccess("ContainerAccess", hasFootprint=[onto.Footprint(NetworkDistance=[0.7])])
+# access_effect = ContainerAccess("ContainerAccess", hasFootprint=[ontontoSpa.Footprint(NetworkDistance=[0.7])])
 #
 # accessibility_impact = ContainerAccessibility("ContainerAccessibility", hasSource=[access_effect])
 #
-# loc5 = onto.Locality("PointInNetwork", geometry=['0.12, v1:v2'])
+# loc5 = ontontoSpa.Locality("PointInNetwork", geometry=['0.12, v1:v2'])
 #
-# container_source = onto.RecyclingContainer("RecyclingContainer", causes=[access_effect], isAt=[loc5])
+# container_source = ontontoSpa.RecyclingContainer("RecyclingContainer", causes=[access_effect], isAt=[loc5])
 #
 # # Most probably a Road Network is an instance of the context with reference to some database table or something like that
 # roadnetwork = RoadNetwork("RoadNetwork")
--- scribble.py
+++ scribble.py
@@ -1,14 +1,14 @@
 from owlready2 import *
 from functions import *
+import rdflib
 
-
-# ontoImp = get_ontology("http://test.org/onto.owl")
 
 onto_path.append('ontologies/')
 ontoImp = get_ontology('http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact').load()
-ontoSpa = get_ontology('http://www.semanticweb.org/rusnesileryte/ontologies/2018/3/spatial').load()
+ontoSpa = get_ontology('http://www.semanticweb.org/rusnesileryte/ontologies/2018/3/mini-spatial').load()
+geo = "http://www.opengis.net/ont/geosparql#"
+geof = "http://www.opengis.net/def/function/geosparql/"
 
-# o = get_ontology("http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact")
 
 def where_is(what, onto):
 
@@ -27,7 +27,7 @@ def where_is(what, onto):
 
 with ontoImp:
 
-    import ontology_impact
+    # import ontology_impact
 
 
     """---------IMPACTS---------"""
@@ -57,47 +57,125 @@ with ontoImp:
     #                 effectzone.hasRoot.append(loc)
 
     """---------MATCHING---------"""
-with ontoSpa:
+# with ontoImp:
 
-    import ontology_spatial
+    # import ontology_spatial
 
-    class Entity(Thing):
-        equivalent_to = [ontoImp.ContextElement |
-                         ontoImp.Effect]
-
-    class hasAnchor(ObjectProperty):
-        equivalent_to = [ontoImp.hasRoot]
-
-    class hasRelation(ObjectProperty):
-        equivalent_to = [ontoImp.hasFootprint]
-
-    class hasLocation(ObjectProperty):
-        equivalent_to = [ontoImp.isAt,
-                         ontoImp.occursAt]
-
-    class SpatialRelation(ontoSpa.Relation):
-        equivalent_to = [ontoImp.Footprint]
+    # class Entity(Thing):
+    #     equivalent_to = [ontoImp.ContextElement |
+    #                      ontoImp.Effect]
+    #
+    # class hasAnchor(ObjectProperty):
+    #     equivalent_to = [o.hasRoot]
+    #
+    # class hasRelation(ObjectProperty):
+    #     equivalent_to = [o.hasFootprint]
+    #
+    # class hasLocation(ObjectProperty):
+    #     equivalent_to = [o.isAt,
+    #                      o.occursAt]
+    #
+    # class SpatialRelation(o.Relation):
+    #     equivalent_to = [o.Footprint]
 
 
 
 sync_reasoner()
 
-print(list(ontoSpa.hasAnchor.get_relations()))
+# print(list(o.hasAnchor.get_relations()))
 # where_is(ontoImp.SulfurEmission, ontoImp)
 # where_is(ontoImp.EffectZone, ontoImp)
 
 
-# print(list(ontoImp.ContextElement.instances()))
+# print(list(ontoImp.Entity.instances()))
+# print(list(ontoImp.classes()))
 # for receiver in ontoImp.ImpactReceiver.instances():
 #     print(receiver)
 # ontoImp.search(affects = "*")
 # for element in ontoImp.ContextElement.instances():
 #     print(element, element.isWithin)
 
+# save to a new ontology
+# ontoImp.save(file = "ontologies/odour", format = "rdfxml")
 
 
+graph = default_world.as_rdflib_graph()
+
+# uri = "http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact"
+# query_res = list(graph.query_owlready("""PREFIX imp: <http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact#>
+#                                          SELECT ?odour ?distance
+#                                          WHERE { ?odour imp:SpreadingDistance ?distance .
+#                                                 FILTER (?distance > 300)}
+#                                          """))
+# query_res = list(graph.query_owlready("""PREFIX imp: <http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact#>
+#                                          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#                                          PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
+#
+#                                          SELECT ?loc
+#                                          WHERE {
+#                                          ?locality imp:Geometry ?loc .
+#                                          ?actor imp:isAt ?locality .
+#                                          ?actor rdf:type imp:Actor .
+#
+#                                          }
+#                                       """))
 
 
+# query_res = list(graph.query_owlready("""PREFIX imp: <http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact#>
+#                                          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#                                          PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
+#
+#                                          SELECT ?geom
+#                                          WHERE {
+#                                          ?locality imp:Geometry ?geom .
+#                                          imp:Westelijk_Havengebied imp:isAt ?locality .
+#                                          }
+#                                       """))
+
+query_res = list(graph.query("""PREFIX my: <http://example.org/ApplicationSchema#>
+                                PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+                                PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
+
+                                SELECT ?f
+
+                                WHERE { ?f my:hasPointGeometry ?fGeom .
+                                ?fGeom geo:asWKT ?fWKT .
+                                FILTER (geof:sfWithin(?fWKT,
+                                "Polygon ((-83.4 34.0, -83.1 34.0, -83.1 34.2, -83.4 34.2, -83.4 34.0))"^^geo:wktLiteral))
+                                      }
+                                      """))
+
+
+# query_res = list(graph.query("""PREFIX imp: <http://www.semanticweb.org/rusnesileryte/ontologies/2018/5/impact#>
+#                                          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+#                                          PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+#                                          PREFIX geof: <http://www.opengis.net/def/function/geosparql/>
+#
+#                                          SELECT (geof:envelope(?pointwkt_lit) as ?bbox) ?pointwkt_lit
+#                                          WHERE {
+#                                          ?poly imp:Geometry ?polywkt .
+#                                          imp:Westelijk_Havengebied imp:isAt ?poly .
+#
+#                                          ?point imp:Geometry ?pointwkt .
+#                                          ?actor imp:isAt ?point .
+#                                          ?actor rdf:type imp:Actor .
+#
+#                                          BIND (STRDT(CONCAT("<http://www.opengis.net/def/crs/EPSG/0/28992>", ?pointwkt), geo:WktLiteral) as ?pointwkt_lit) .
+#                                          BIND (STRDT(?polywkt, geo:WktLiteral) as ?polywkt_lit)
+#                                          }
+#                                       """))
+
+# FILTER (geof:sfWithin(?pointwkt_lit, "<http://www.opengis.net/def/crs/OGC/1.3/CRS84>
+#                                        Polygon ((-83.4 34.0, -83.1 34.0,
+#                                        -83.1 34.2, -83.4 34.2,
+#                                        -83.4 34.0))"^^geo:wktLiteral))
+
+
+print(query_res)
+# print(len(graph))
+# for s, p, o in graph:
+#     print(s, p, o)
+    # break
 
 
 # hasFootprint some (BufferFootprint and SpreadingDistance value 10000 and Unit value "m")
